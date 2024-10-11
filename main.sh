12182337fe12e526798bf52bf721e83857094549
lucas <ltech.corpore@gmail.com>
2024-10-11 20:49:41 -0300

    lucas/update correção

--- main.sh
+++ main.sh
@@ -19,9 +19,9 @@ update(){
 read -p "Qual arquivo você deseja editar ?(sem txt)" file
 if [[ -f "${file}.txt" ]]; then
 	read -p "Digite o conteúdo do Arquivo!" conteudo
-	echo "${conteudo}" > "${file}".txt
+	echo "${conteudo}" > "${file}.txt"
 else
-	echo "O arquivo ${file}txt não existe"
+	echo "O arquivo ${file}.txt não existe"
 fi
 }
 
